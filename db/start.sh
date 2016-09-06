d82c4a9b57a3494316fc13edda4d94f770464728
Daniel Kenner <daniel@dansmac.local>
2016-09-06 02:04:30 -0600

    initial commit

---
+++ db/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+service mysql start
+
+
+
+#sleep forevermore to let services do their job.
+while [ 1 -eq 1 ]
+do
+  sleep 300
+done
---
+++ web/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+service apache2 start
+
+
+
+#sleep forevermore to let services do their job.
+while [ 1 -eq 1 ]
+do
+  sleep 300
+done
